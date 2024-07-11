0cca930adc807dd620e53663f0ce26db1f67504a
durianbeetroot <durianbeetroot@gmail.com>
2024-07-11 18:03:35 +0700

    added chart js and donut graph

--- woody-app/app/admin/layout.js
+++ woody-app/app/admin/layout.js
@@ -8,17 +8,20 @@ const Layout = ({ children }) => {
       <Row>
         <Col md={1} className="vh-100 d-flex flex-column align-items-center justify-content-start" 
             style={{ 
-                backgroundImage: `url(${"/img/shade.png" })`,
-                boxShadow: '0 4px 16px rgba(0, 0, 0, 0.4)'
-        }}>
+                    backgroundImage: `url(${"/img/shade.png" })`,
+                    boxShadow: '0 4px 16px rgba(0, 0, 0, 0.4)'
+            }}>
              <div className="d-flex flex-column align-items-center pt-4 space-y-3 w-full">
                 <Image src="/img/logo.png" alt="Logo" width={50} height={50} className="object-cover pb-4"/>
-                <Link href="/admin" className="f-2 py-1 text-md no-underline txt-sidebar duration-300 w-full text-center">Dashboard</Link>
-                <Link href="/admin/products" className="f-2 py-1 text-md no-underline txt-sidebar duration-300 w-full text-center">Products</Link>
-                <Link href="/admin/reviews" className="f-2 py-1 text-md no-underline txt-sidebar duration-300 w-full text-center">Reviews</Link>
+                <Link href="/admin" className="f-2 py-1 text-sm no-underline txt-sidebar duration-300 w-full text-center">Dashboard</Link>
+                <Link href="/admin/products" className="f-2 py-1 text-sm no-underline txt-sidebar duration-300 w-full text-center">Products</Link>
+                <Link href="/admin/reviews" className="f-2 py-1 text-sm no-underline txt-sidebar duration-300 w-full text-center">Reviews</Link>
             </div>
         </Col>
-        <Col md={11}>
+        <Col md={11}
+        style={{ 
+          backgroundImage: `url(${"/img/shadeWhite.png" })`
+      }}>
           {children}
         </Col>
       </Row>
--- woody-app/app/admin/page.js
+++ woody-app/app/admin/page.js
@@ -1,8 +1,53 @@
+"use client"
+
+import { Container,Row,Col } from "react-bootstrap";
+import React from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function Page(){
+
+    const data = {
+        labels: ['Positive', 'Negative', 'Neutral','Undecided'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 23],
+            backgroundColor: [
+                '#c0c0c0',
+                '#404040',
+                '#808080',
+                '#D6A56E'
+              ]
+          },
+        ],
+      };
+    
+      const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'bottom',
+          },
+          tooltip: {
+            enabled: true,
+          },
+        },
+      };
+
     return (
-        <h1>
-            test
-        </h1>
+        <Container>
+            <Row className="pt-4">
+                <h1 className="f-2 font-semibold text-4xl txt-1 drop-shadow-md"> Dashboard </h1>
+            </Row>
+            <Row className="pt-4">
+                <Col md={4} className="bg-white p-2 rounded-lg shadow-xl">
+                    <h1 className="f-2 p-2 text-2xl drop-shadow-xl">Reviews Comparison (%)</h1>
+                    <Doughnut data={data} options={options} />
+                </Col>
+            </Row>
+        </Container>
     )
 }
